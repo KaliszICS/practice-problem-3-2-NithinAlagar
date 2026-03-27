9e019821a5a4de72616f93ecd11d597f22297df4
Nithin Alagar <343954178@tdsb.ca>
2026-03-27 18:14:10 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,38 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static isFive(int num){
+		if (num == 5){
+			return "The number is Five";
 
+		}
+		else {
+			return "The number is not Five";
+		}
+
+	}
+
+	public static positiveOrNegative(double num){
+		if (num>0){
+			return "Positive";
+		}
+		else {
+			return "Negative";
+		}
+		
+	}
+
+	public static highOrLow(int num) {
+		if (num>100){
+			return "High";
+		}
+		else {
+			return "Low";
+		}
+	}
+	public static isHello(String word){
+		if (word.equals("Hello")){
+			return "The word is Hello"
+		}
+	}
 }
